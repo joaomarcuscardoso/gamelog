769ee4a55a6b8c51de8c160ba017a9523e8d1e53
joaomarcuscardoso <joao.cardoso@tikiweb.com.br>
2024-01-10 22:53:09 -0300

    init project

---
+++ src/main/java/com/udesc/padroesdeprojeto/gamelog/GamelogApplication.java
@@ -0,0 +1,13 @@
+package com.udesc.padroesdeprojeto.gamelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udesc/padroesdeprojeto/gamelog/GamelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udesc.padroesdeprojeto.gamelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
