e5b035240bba8ccf9f43fe282b0bf6df095b9a76
joaomarcuscardoso <joao.cardoso@tikiweb.com.br>
2024-01-14 16:21:32 -0300

    primeiro index da API

--- src/main/java/com/udesc/padroesdeprojeto/gamelog/controller/UserController.java
+++ src/main/java/com/udesc/padroesdeprojeto/gamelog/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.udesc.padroesdeprojeto.gamelog.controller;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
+
+    @GetMapping
+    public ResponseEntity<Object> index() {
+        String content = "ok";
+        return ResponseEntity.status(HttpStatus.OK).body(content);
+    }
 }
