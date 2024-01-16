bbda4472e43bfd619265fb77d46ee2bd9a55487a
joaomarcuscardoso <rfcjoujou@gmail.com>
2024-01-16 18:44:19 -0300

    validação de usuário

--- src/main/java/com/udesc/padroesdeprojeto/gamelog/controller/GameController.java
+++ src/main/java/com/udesc/padroesdeprojeto/gamelog/controller/GameController.java
@@ -6,10 +6,12 @@ import com.udesc.padroesdeprojeto.gamelog.model.Game;
 import com.udesc.padroesdeprojeto.gamelog.model.User;
 import com.udesc.padroesdeprojeto.gamelog.repository.GameRepository;
 import com.udesc.padroesdeprojeto.gamelog.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -17,6 +19,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -26,12 +29,6 @@ import java.util.List;
 public class GameController {
     private final GameRepository gameRepository;
     private final UserRepository userRepository;
-    @RequestMapping
-    public ModelAndView index(Authentication authentication, Model model) {
-        model.addAttribute("message", "This message is from the controller.");
-        return new ModelAndView("home");
-
-    }
     @GetMapping
     public ResponseEntity<List<Game>> listAll(){
         return ResponseEntity.status(HttpStatus.OK).body(gameRepository.findAll());
@@ -64,5 +61,33 @@ public class GameController {
         return ResponseEntity.status(HttpStatus.OK).body(game);
     }
 
+    @GetMapping
+    public ModelAndView index(Model model, HttpSession session, RedirectAttributes redirectAttributes) {
+        User user = this.validate(model, session, redirectAttributes);
+        if (user == null) {
+            redirectAttributes.addFlashAttribute("error", "Authenticação falhou!");
+            return new ModelAndView("redirect:/auth/signin");
+        }
 
+        return new ModelAndView("home");
+    }
+
+    private User validate(Model model, HttpSession session, RedirectAttributes redirectAttributes) {
+        String token = (String) session.getAttribute("token");
+        String email = (String) session.getAttribute("email");
+//        System.out.println("TOKEN: " + token);
+//        System.out.println("EMAIL: " + email);
+        if (session.getAttribute("token") == null || session.getAttribute("email") == null)
+            return null;
+
+        User user = userRepository.findByToken(token).orElse(null);
+        User userEmail = userRepository.findByEmail(email).orElse(null);
+        if (user == null || userEmail == null)
+            return null;
+
+        if (!user.getEmail().equals(userEmail.getEmail()))
+            return null;
+
+        return user;
+    }
 }
