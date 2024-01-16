610ca5940693f9da1e97c1460935c9c2a9a03e62
joaomarcuscardoso <rfcjoujou@gmail.com>
2024-01-16 18:33:24 -0300

    criando login, registro de usuário e validação de token e email

--- src/main/java/com/udesc/padroesdeprojeto/gamelog/controller/GameController.java
+++ src/main/java/com/udesc/padroesdeprojeto/gamelog/controller/GameController.java
@@ -1,6 +1,11 @@
 package com.udesc.padroesdeprojeto.gamelog.controller;
 
+import com.udesc.padroesdeprojeto.gamelog.model.User;
+import com.udesc.padroesdeprojeto.gamelog.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -9,14 +14,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
 @RequestMapping("/games")
 public class GameController {
-    @RequestMapping
-    public ModelAndView index(Authentication authentication, Model model) {
-        model.addAttribute("message", "This message is from the controller.");
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user;
+    @GetMapping
+    public ModelAndView index(Model model, HttpSession session, RedirectAttributes redirectAttributes) {
+        User user = this.validate(model, session, redirectAttributes);
+        if (user == null) {
+            redirectAttributes.addFlashAttribute("error", "Authenticação falhou!");
+            return new ModelAndView("redirect:/auth/signin");
+        }
+
         return new ModelAndView("home");
+    }
+
+    private User validate(Model model, HttpSession session, RedirectAttributes redirectAttributes) {
+        String token = (String) session.getAttribute("token");
+        String email = (String) session.getAttribute("email");
+//        System.out.println("TOKEN: " + token);
+//        System.out.println("EMAIL: " + email);
+        if (session.getAttribute("token") == null || session.getAttribute("email") == null)
+            return null;
+
+        User user = userRepository.findByToken(token).orElse(null);
+        User userEmail = userRepository.findByEmail(email).orElse(null);
+        if (user == null || userEmail == null)
+            return null;
+
+        if (!user.getEmail().equals(userEmail.getEmail()))
+            return null;
+
+        return user;
     }
 }
--- src/main/java/com/udesc/padroesdeprojeto/gamelog/controller/UserController.java
+++ src/main/java/com/udesc/padroesdeprojeto/gamelog/controller/UserController.java
@@ -1,18 +1,33 @@
 package com.udesc.padroesdeprojeto.gamelog.controller;
 
+import com.udesc.padroesdeprojeto.gamelog.dto.LoginRequest;
+import com.udesc.padroesdeprojeto.gamelog.dto.SignupRequest;
+import com.udesc.padroesdeprojeto.gamelog.model.User;
+import com.udesc.padroesdeprojeto.gamelog.repository.UserRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/auth")
 public class UserController {
+
+    @Autowired
+    private UserRepository repository;
 
     @GetMapping("/signin")
     public ModelAndView signin() {
@@ -23,5 +38,47 @@ public class UserController {
     public ModelAndView signup() {
 
         return new ModelAndView("signup");
+    }
+
+    @PostMapping("/signin")
+    public String authenticateUser(@Valid @ModelAttribute("loginRequest") LoginRequest loginRequest,
+                                   RedirectAttributes redirectAttributes, HttpSession session) {
+        User user = repository.findByUsername(loginRequest.getUsername()).orElse(null);
+        if (user == null) {
+            redirectAttributes.addFlashAttribute("error", "Login Failed!");
+            return "redirect:/auth/signin";
+        }
+
+        PasswordEncoder encode = new BCryptPasswordEncoder();
+        if (encode.matches(loginRequest.getPassword(), user.getPassword())) {
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            repository.save(user);
+            session.setAttribute("token", token);
+            session.setAttribute("email", user.getEmail());
+            redirectAttributes.addFlashAttribute("message", "Login successful!");
+
+            return "redirect:/games";
+        } else {
+            redirectAttributes.addFlashAttribute("error", "Login Failed!");
+            return "redirect:/auth/signin";
+        }
+    }
+
+    @PostMapping("/signup")
+    public String processSignupForm(@ModelAttribute("signupRequest") SignupRequest signupRequest, RedirectAttributes redirectAttributes) {
+        Boolean existUser = repository.existsByUsernameAndEmail(signupRequest.getUsername(), signupRequest.getEmail());
+        if (existUser) {
+            redirectAttributes.addFlashAttribute("error", "Error Ao Criar usuário!");
+            return "redirect:/auth/signup";
+        }
+
+        PasswordEncoder encode = new BCryptPasswordEncoder();
+        String passwordEncode = encode.encode(signupRequest.getPassword());
+
+        User user = new User(signupRequest.getUsername(), signupRequest.getEmail(), passwordEncode);
+        repository.save(user);
+        // Redirect to a success page or login page
+        return "redirect:/auth/signin";
     }
 }
