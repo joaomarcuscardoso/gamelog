3253a0d38ba781ddeee14558b27469de6b4dd72a
Igor Meurer <igorigormeurer@hotmail.com>
2024-01-21 21:45:57 -0300

    Quase nada

--- src/main/java/com/udesc/padroesdeprojeto/gamelog/configuration/JavaMailConfiguration.java
+++ src/main/java/com/udesc/padroesdeprojeto/gamelog/configuration/JavaMailConfiguration.java
@@ -21,7 +21,7 @@ public class JavaMailConfiguration {
 
         Properties props = javaMailSenderImpl.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");  // Correção aqui
+        props.put("mail.smtp.auth", "true"); 
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "true");
 
